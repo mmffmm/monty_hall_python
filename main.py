9c0fc8c871b50d0b70be12407b94b3b7847ad417
Fad <fathullahmuhammad300@gmail.com>
2025-01-08 23:37:18 +0800

    Remove useless process, make total box more dynamic

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-# test
-# test
 import random
 
+# Setup
 number_of_simulations = 100000
 change_answer = True
+total_boxes = 100
+
+
 win = 0
 lose = 0
 
 for i in range(number_of_simulations):
-    # prize
-    prize = random.randint(0,2)
+
+    prize = random.randint(1,total_boxes)
     # print("Prize: ", prize)
 
-    # pick
-    pick = random.randint(0,2)
+    pick = random.randint(1,total_boxes)
     # print("Pick: ", pick)
 
-    # remove
-    remove = random.randint(0,2)
-    while(remove == prize or remove == pick): remove = random.randint(0,2)
+    # NO NEED REMOVE LOL
+    remove = random.randint(1,total_boxes)
+    while(remove == prize or remove == pick): remove = random.randint(1,total_boxes)
     # print("Remove" , remove)
 
-    # change
     if change_answer:
-        for j in range(3):
-            if j != pick and j != remove:
-                pick = j
-                break
+        if pick == prize:
+            lose += 1
+        else:
+            win += 1
 
-    if pick == prize:
-        # print("win")
-        win+=1
-    else:
-        # print("lose")
-        lose+=1
+    # if pick == prize:
+    #     # print("win")
+    #     win+=1
+    # else:
+    #     # print("lose")
+    #     lose+=1
 
 print("Win: ", win)
 print("Lose: ", lose)
